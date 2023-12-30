61c91d86512dfccf5489a026bae064bf2975d54a
jramber <jdramirezbernal@gmail.com>
2023-12-30 16:34:04 +0100

    added pop-up element

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ const validateEmail = (email) => String(email)
     .toLowerCase()
     .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
-async function sendData() {
+async function sendData(email) {
     const formData = new FormData();
 
-    formData.append("EMAIL", "example@gmail.ccom");
+    formData.append("EMAIL", email);
 
     try {
         const response = await fetch("https://gmail.us8.list-manage.com/subscribe/post?u=02164d34cf05821b94a23aab3&amp;id=69236752be&amp;f_id=006fd0e3f0", {
@@ -28,23 +28,14 @@ async function sendData() {
     }
 }
 
-// const send = document.querySelector("#send");
-// send.addEventListener("click", sendData);
-
-
 email_submit_button.addEventListener('click', async () => {
     if(email_input.value === "") {
         return;
     }
 
     if(validateEmail(email_input.value)){
-        console.log('hey');
         await sendData();
     }
-
-    // const input = document.createElement('input');
-    // input.type = 'email';
-    // input.value = form_email.value;
 });
 
 // TODO: listen to return key pressed
